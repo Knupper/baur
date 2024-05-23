03d7926b2e25b9609cc5befd279556eead46994e
Max Steffen <max.steffen@mail.com>
2024-05-23 12:35:53 +0200

    fix lint issues and add mock main

--- lib/main_mock.dart
+++ lib/main_mock.dart
@@ -1,18 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_baur/data/data_sources/advice_remote_data_source.dart';
-import 'package:flutter_baur/data/repositories/advice_repository_rest.dart';
+import 'package:flutter_baur/data/repositories/advice_repository_mock.dart';
 import 'package:flutter_baur/domain/repositories/advice_repository.dart';
 import 'package:flutter_baur/presentation/my_app.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MultiRepositoryProvider(
     providers: [
       RepositoryProvider<AdviceRepository>(
-        create: (context) => AdviceRepositoryRest(
-          dataSource: AdviceRestApiDataSource(client: http.Client()),
-        ),
+        create: (context) => AdviceRepositoryMock(),
       ),
     ],
     child: const MyApp(),
